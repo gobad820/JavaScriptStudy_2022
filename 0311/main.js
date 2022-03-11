cd979ad8a00f188973c967cb7db28c3017410cc2
rlarbqor00 <rlarbqor00@naver.com>
2022-03-11 15:29:56 +0900

    2회차

--- 0311/main.js
+++ 0311/main.js
@@ -1,27 +1,38 @@
-// const slider = document.getElementById("slider");
 const slider = document.querySelector("#slider");
 const inner = document.querySelector("#slider > .inner");
-const imges = document.querySelectorAll("#slider > .inner > li");
+const imgs = document.querySelectorAll("#slider > .inner > li");
 const left = document.querySelector("#left");
 const right = document.querySelector("#right");
 
-
-(function clone_img() {
-    for(let i= 0; i<imges.length; i++) {
-        const clone = imges[i].cloneNode(true);
-        imges[0].before(clone);
-    }
-    for(let i = imges.length; i > 0; i--) {
-        const clone = imges[i-1].cloneNode(true);
-        imges[imges.length-1].after(clone);
-
-    }
+(function clone_img() { 
+	for (let i = 0; i < imgs.length; i++) {
+		const clone = imgs[i].cloneNode(true);
+		imgs[0].before(clone);
+	}
+	for (let i = imgs.length; i > 0; i--) {
+		const clone = imgs[i - 1].cloneNode(true);
+		imgs[imgs.length - 1].after(clone);
+	}
 })();
 
-left.addEventListener("click", ()=> {
-    console.log("left");
+// slider event
+var count = imgs.length;
+right.addEventListener("click", () => {
+    count++;
+    console.log(count);
+    if(count === imgs.length*2) {
+        inner.style.transform = `translate(calc(-120px*${imgs.length}))`;
+        count = imgs.length;
+    }
+    inner.style.transform = `translate(calc(-120px*${count}))`
 });
-
-right.addEventListener("click", ()=>{
-    console.log("right");
+left.addEventListener("click", () => {
+    count--;
+    console.log(count);
+    if(count === 0) {
+        inner.style.transform = `translate(calc(-120px*${imgs.length}))`;
+        count = imgs.length;
+    }
+    inner.style.transform = `translate(calc(-120px*${count}))`
+    
 });
